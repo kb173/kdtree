167e8f9112b0802a2b91c1f331c850b3cdeee985
Nico <33263148+NicoPlattner@users.noreply.github.com>
2018-06-03 18:55:40 +0200

    input

--- main.cpp
+++ main.cpp
@@ -1,29 +1,65 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "kdtree.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
 
     auto *tree = new kdtree();
 
-    int p1[2] = {2, 2}; // x
-    int p2[2] = {1, 1}; // y
-    int p3[2] = {3, 1}; // x
-    int p4[2] = {4, -1};// y
+    if (argc > 1) {
+        cout << "argv[1] = " << argv[1] << endl;
+    } else {
+        cout << "No file name entered. Exiting...";
+        return -1;
+    }
 
-    tree->insert(p1);
-    tree->insert(p2);
-    tree->insert(p3);
-    tree->insert(p4);
 
-    int p5[2] = {1, 0};
+    ifstream file;
+    file.open(argv[1]);
 
-    int **result = tree->search(p5, 2);
+    int number;
+    bool isx;
 
-    for (int i = 0; i < 2; i++) {
+    int xvalue;
+    int yvalue;
+
+    file >> number;
+
+    while (file >> number) {
+        if(isx){
+            xvalue = number;
+            isx = !isx;
+        } else{
+            yvalue = number;
+            isx = !isx;
+
+            int point[2] = {xvalue, yvalue};
+            tree->insert(point);
+        }
+    }
+
+    int searchx;
+    int searchy;
+    int amount;
+
+    cout << "Enter the point you want to search for: " << endl;
+
+    cin >> searchx;
+    cin >> searchy;
+
+    cout << "How many points do you want to search for in its surroundings" << endl;
+
+    cin >> amount;
+
+    int searchpoint[2] = {searchx, searchy};
+    int **result = tree->search(searchpoint, amount);
+
+
+    for (int i = 0; i < amount; i++) {
         cout << "Point found: ";
 
-        for (int d = 0; d < 2; d++) {
+        for (int d = 0; d < amount; d++) {
             cout << result[i][d] << ", ";
         }
 
