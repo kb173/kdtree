d17995a1cff505f0d94806465e17e183c3adfa0b
kb173 <karl.bittner@outlook.com>
2018-06-05 10:41:39 +0200

    Proper behaviour when less points than entered are found

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kdtree
+
+Zum ausführen: kdtree in cmake-build-debug mit points.txt (oder anderer Liste von Punkten) als command line argument öffnen
--- main.cpp
+++ main.cpp
@@ -77,13 +77,16 @@ int main(int argc, char *argv[]) {
 
     for (int i = 0; i < amount; i++)
     {
-        cout << "Point found: ";
-
-        for (int d = 0; d < 2; d++)
+        if (result[i] != nullptr)
         {
-            cout << result[i][d] << ", ";
+            cout << "Point found: ";
+
+            for (int d = 0; d < 2; d++)
+            {
+                cout << result[i][d] << ", ";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
 
     // create the window
@@ -125,8 +128,11 @@ int main(int argc, char *argv[]) {
 
         for (int i = 0; i < amount; i++)
         {
-            found_p.setPosition(720 + result[i][0], 720 + result[i][1]);
-            window.draw(found_p);
+            if (result[i] != nullptr)
+            {
+                found_p.setPosition(720 + result[i][0], 720 + result[i][1]);
+                window.draw(found_p);
+            }
         }
 
         // end the current frame
@@ -135,123 +141,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
-
-/*
-int main()
-{
-    auto *tree = new kdtree();
-
-    int amount = 6;
-
-    auto **points = new double*[amount];
-
-    for(int i = 0; i<amount; i++){
-        points[i] = new double[2];
-    }
-
-    points[0][0] = 200;
-    points[1][0] = -300;
-    points[2][0] = 300;
-    points[3][0] = -200;
-    points[4][0] = 10;
-    points[5][0] = -100;
-
-    points[0][1] = 200;
-    points[1][1] = 0;
-    points[2][1] = 0;
-    points[3][1] = -150;
-    points[4][1] = 50;
-    points[5][1] = 50;
-
-  //  {{200, 200}, {-300, 0}, {300, 0}, {-200, -150}, {10, 50}, {-100, 50}};
-*/
-/*
-    for (int i = 0; i < 6; i++)
-    {
-        tree->insert(points, 6);
-    }
-    *//*
-
-
-    tree->insert(points, amount, 0);
-
-    tree->print();
-
-    double search_point[2] = {110, 400};
-
-    auto **result = new double*[3];
-
-    for (int i = 0; i < 2; i++)
-    {
-        result[i] = new double[2];
-    }
-
-    result = tree->search(search_point, 3);
-
-    for (int i = 0; i < 3; i++)
-    {
-        cout << "Point found: ";
-
-        for (int d = 0; d < 2; d++)
-        {
-            cout << result[i][d] << ", ";
-        }
-
-        cout << endl;
-    }
-
-    */
-/*
-
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(1440, 1440), "k-d-tree");
-
-    sf::CircleShape normal_p(10);
-    normal_p.setFillColor(sf::Color(250, 250, 250));
-
-    sf::CircleShape search_p(10);
-    search_p.setFillColor(sf::Color(250, 100, 50));
-
-    sf::CircleShape found_p(10);
-    found_p.setFillColor(sf::Color(100, 250, 50));
-
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-        for (int i = 0; i < 6; i++)
-        {
-            normal_p.setPosition(720 + points[i][0], 720 + points[i][1]);
-            window.draw(normal_p);
-        }
-
-        search_p.setPosition(720 + search_point[0], 720 + search_point[1]);
-        window.draw(search_p);
-
-        for (int i = 0; i < 3; i++)
-        {
-            found_p.setPosition(720 + result[i][0], 720 + result[i][1]);
-            window.draw(found_p);
-        }
-
-        // end the current frame
-        window.display();
-    }
-
-     *//*
-
-    return 0;
-}*/
